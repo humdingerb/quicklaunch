11ae0192ff4f2562a0cec28375963496ad545d68
Humdinger <humdingerb@gmail.com>
2022-03-03 09:05:57 +0100

    Add directories src and artwork
    To tidy up the top directory...

--- Add to Deskbar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Add QuickLaunch to the Deskbar tray
-
-cd $(dirname "$0")
-desklink "cmd=Remove replicant:desklink --remove=QuickLaunch" `pwd`/QuickLaunch
-alert --info "QuickLaunch was added to the Deskbar.
-
-You can remove it again with its context menu (a right-click on its icon)." OK
