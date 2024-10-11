270cfe86737b2f5518477f31d81066bce4b66061
Humdinger <humdinger@mailbox.org>
2024-10-11 19:45:13 +0200

    Support localized application names
    A query is only able to to find the English original name of apps.
    
    Instead of doing a query every time the user enters a letter, we now do one
    query for all applications (minus the ones from the ignore list) when creating
    the MainWindow (see BuildAppList()) and put them into fAppList.
    
    If "Translate app names" is activated in Haiku's Locale prefs, we use the
    localized filenames instead of the regular names.
    
    When a letter is entered, we empty the MainListView and only add the matching
    items from fAppList (see FilterAppList(), previously named BuildList()).
    
    When the ignore list changes or is de/activated we empty fAppList and call
    BuildAppList() to create a new one.

---
+++ src/AppListItem.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2024. All rights reserved.
+ * Distributed under the terms of the MIT license.
+ *
+ * Author:
+ *	Humdinger, humdingerb@gmail.com
+ */
+
+#include "AppListItem.h"
+
+#include <LocaleRoster.h>
+
+
+AppListItem::AppListItem(BEntry entry, bool localized)
+{
+	entry.GetRef(&fRef);
+	fName = fRef.name;
+
+	if (localized) {
+		if (BLocaleRoster::Default()->GetLocalizedFileName(fName, fRef) != B_OK)
+			fName = fRef.name;
+	}
+
+}
---
+++ src/AppListItem.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024. All rights reserved.
+ * Distributed under the terms of the MIT license.
+ *
+ * Author:
+ *	Humdinger, humdingerb@gmail.com
+ */
+#ifndef APPLISTITEM_H
+#define APPLISTITEM_H
+
+
+#include <Entry.h>
+#include <String.h>
+
+
+class AppListItem {
+public:
+	AppListItem(BEntry entry, bool localized);
+
+	BString GetName() { return fName; };
+	entry_ref* GetRef() { return &fRef; };
+	
+private:
+	entry_ref fRef;
+	BString fName;
+};
+
+#endif // APPLISTITEM_H
